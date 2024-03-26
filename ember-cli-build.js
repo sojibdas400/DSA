b94bf5f5a52faac48007be6fb0ba8bf84263a7d3
sojib600 <sojibdas400@gmail.com>
2024-03-26 18:33:40 +0600

    bootstrap added

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,5 +7,11 @@ module.exports = function (defaults) {
     // Add options here
   });
 
+  // ember-cli-build.js
+  app.import('node_modules/jquery/dist/jquery.js');
+  app.import('node_modules/popper.js/dist/umd/popper.js');
+  app.import('node_modules/popper.js/dist/umd/popper-utils.js');
+  app.import('node_modules/bootstrap/dist/js/bootstrap.js');
+
   return app.toTree();
 };
