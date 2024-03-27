dfa8eedcd2e9c9d91e3162ca75b67949a92429d3
sojib600 <sojibdas400@gmail.com>
2024-03-27 11:56:51 +0600

    stacks tabs showing

--- app/components/stacks.js
+++ app/components/stacks.js
@@ -1,3 +1,40 @@
 import Component from '@glimmer/component';
 
-export default class StacksComponent extends Component {}
+export default class StacksComponent extends Component {
+  names = ['index', 'methods', 'properties'];
+
+  index = {
+    methods: ['push', 'pop', 'top', 'size', 'isEmpty'],
+    properties: ['test'],
+  };
+
+  methods = [
+    {
+      name: 'push',
+      description:
+        'Adds an item to the stack. If the stack is full, then it is said to be an Overflow condition.',
+      code: `
+      import Component from '@glimmer/component';
+
+      export default class StacksComponent extends Component {
+        names = ['index', 'methods', 'properties'];
+
+        properties = [];
+        index = {
+          methods: ['push', 'pop', 'top', 'size', 'isEmpty'],
+          properties: ['test'],
+        };
+
+        methods = [
+          {
+            name: 'push',
+            description:
+              'Adds an item to the stack. If the stack is full, then it is said to be an Overflow condition.',
+          },
+        ];
+      }`,
+    },
+  ];
+
+  properties = [];
+}
